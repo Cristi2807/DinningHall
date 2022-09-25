cf0add901c195a8e2f42bc9183f118d7385fae4f
Cristi <cristian.boris@isa.utm.md>
2022-09-25 21:07:52 +0300

    Implement tables logic

--- src/tables.go
+++ src/tables.go
@@ -1,1 +1,61 @@
 package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+type Table struct {
+	isFree        bool
+	wantsToOrder  bool
+	waitingOrder  bool
+	orderReceived bool
+	order         Order
+	m             sync.Mutex
+}
+
+var tables [NrOfTables]Table
+
+func InitTables() {
+	for i := 0; i < NrOfTables; i++ {
+		tables[i].isFree = true
+	}
+}
+
+func OperateTable(tableNr int) {
+	tables[tableNr].m.Lock()
+	defer tables[tableNr].m.Unlock()
+
+	if tables[tableNr].isFree {
+		tables[tableNr].isFree = false
+		time.Sleep(time.Duration(2*TIMEUNIT) * time.Millisecond)
+
+	} else if !tables[tableNr].wantsToOrder && !tables[tableNr].waitingOrder {
+		tables[tableNr].wantsToOrder = true
+		fmt.Printf("Table %d wants to order\n", tableNr)
+		jobsTake <- tableNr
+
+	} else if tables[tableNr].orderReceived {
+		fmt.Printf("Table %d received order after %s\n", tableNr, time.Now().Sub(tables[tableNr].order.PickUpTime))
+		time.Sleep(time.Duration(10*TIMEUNIT) * time.Millisecond)
+		tables[tableNr].order = Order{}
+		tables[tableNr].orderReceived = false
+		tables[tableNr].isFree = true
+		tables[tableNr].waitingOrder = false
+	}
+}
+
+func HandleTable(tableNr int) {
+	s1 := rand.NewSource(time.Now().UnixNano())
+	r1 := rand.New(s1)
+
+	randomInt := r1.Intn(21)
+
+	time.Sleep(time.Duration(randomInt*TIMEUNIT) * time.Millisecond)
+
+	for {
+		OperateTable(tableNr)
+	}
+}
